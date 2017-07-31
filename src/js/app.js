13404b4bdf29c0d250e26c3f0b7eb765179765a2
mrhudson <protegegod@gmail.com>
2017-08-01 00:37:09 +0200

    mgmt

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import css from './style.styl'
-
-import Layout from './components/Layout'
-
-const app = document.getElementById('app')
-ReactDOM.render(<Layout />, app)
